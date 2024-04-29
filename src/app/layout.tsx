0a3829e17328856f3678acd499a77dc51519b5c8
Ed Castro <ed.sdr@outlook.com>
2024-04-29 18:09:16 -0300

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import "~/styles/globals.css";
+
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const metadata = {
+  title: "Commune Wallet",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`font-sans ${inter.variable}`}>{children}</body>
+    </html>
+  );
+}
