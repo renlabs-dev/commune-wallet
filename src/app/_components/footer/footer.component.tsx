60d7ce9a4541dc743b664568ec284b364cc498c8
Ed Castro <ed.sdr@outlook.com>
2024-05-19 16:51:39 -0300

    feat: list of validators component

--- src/app/_components/footer/footer.component.tsx
+++ src/app/_components/footer/footer.component.tsx
@@ -3,7 +3,7 @@ import { Icon } from "..";
 export const Footer = () => {
   return (
     <footer className="flex w-full flex-col items-center justify-between gap-3 border-t border-white bg-black bg-opacity-50 px-12 py-8 text-gray-300 md:flex-row">
-      <p>Renovating the way we build software for everyone.</p>
+      <p>Making decentralized AI for everyone.</p>
 
       <div className="flex gap-4">
         <Icon
@@ -17,13 +17,18 @@ export const Footer = () => {
           href="https://twitter.com/communeaidotorg"
         />
         <Icon
+          src="/icons/telegram.svg"
+          className="h-6 w-6"
+          href="https://t.me/communecommunity"
+        />
+        <Icon
           src="/icons/discord.svg"
           className="h-6 w-6"
           href="https://discord.com/invite/A8JGkZ9Dmm"
         />
       </div>
 
-      <p>© 2024 Commune, Inc. All rights reserved.</p>
+      <p>© 2024 Commune, Inc. no rights reserved.</p>
     </footer>
   );
 };
